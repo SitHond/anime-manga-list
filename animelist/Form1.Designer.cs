e2e94d8923594da963ba2a229e7b37e2bb8b8bca
SitHond <75142057+SitHond@users.noreply.github.com>
2022-11-07 22:00:41 +0300

    Добавьте файлы проекта.

---
+++ animelist/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace animelist
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.field_login = new System.Windows.Forms.TextBox();
+            this.field_pass = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // field_login
+            // 
+            this.field_login.Location = new System.Drawing.Point(295, 136);
+            this.field_login.Name = "field_login";
+            this.field_login.Size = new System.Drawing.Size(100, 20);
+            this.field_login.TabIndex = 0;
+            this.field_login.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // field_pass
+            // 
+            this.field_pass.Location = new System.Drawing.Point(295, 173);
+            this.field_pass.Name = "field_pass";
+            this.field_pass.Size = new System.Drawing.Size(100, 20);
+            this.field_pass.TabIndex = 1;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(307, 216);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(75, 23);
+            this.btn_login.TabIndex = 2;
+            this.btn_login.Text = "Войти";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.field_pass);
+            this.Controls.Add(this.field_login);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox field_login;
+        private System.Windows.Forms.TextBox field_pass;
+        private System.Windows.Forms.Button btn_login;
+    }
+}
+
---
+++ animelist/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace animelist
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(field_login.Text == "a" && field_pass.Text == "a")
+            {
+                Form1 f = new Form1();
+                f.Show();
+                Close();    
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {}
+    }
+}
