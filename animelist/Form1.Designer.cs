88497145f7d8c0f61ff01e433948525fee4ebee1
SitHond <75142057+SitHond@users.noreply.github.com>
2022-11-11 18:57:40 +0300

    CRT

--- animelist/Form1.Designer.cs
+++ animelist/Form1.Designer.cs
@@ -31,18 +31,26 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.user_name = new System.Windows.Forms.Label();
             this.your_list = new System.Windows.Forms.Label();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.anime = new System.Windows.Forms.LinkLabel();
             this.ani_look = new System.Windows.Forms.LinkLabel();
             this.ani_look2 = new System.Windows.Forms.LinkLabel();
             this.ani_viewed = new System.Windows.Forms.LinkLabel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.LoginBox = new System.Windows.Forms.TextBox();
+            this.PassBox = new System.Windows.Forms.TextBox();
+            this.Login_btn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(16, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(68, 62);
+            this.pictureBox1.Size = new System.Drawing.Size(60, 60);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -66,16 +74,16 @@
             this.your_list.TabIndex = 2;
             this.your_list.Text = "Ваш список";
             // 
-            // linkLabel1
+            // anime
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
-            this.linkLabel1.Location = new System.Drawing.Point(16, 104);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(40, 13);
-            this.linkLabel1.TabIndex = 3;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Аниме";
+            this.anime.AutoSize = true;
+            this.anime.LinkColor = System.Drawing.Color.Black;
+            this.anime.Location = new System.Drawing.Point(16, 104);
+            this.anime.Name = "anime";
+            this.anime.Size = new System.Drawing.Size(40, 13);
+            this.anime.TabIndex = 3;
+            this.anime.TabStop = true;
+            this.anime.Text = "Аниме";
             // 
             // ani_look
             // 
@@ -110,22 +118,83 @@
             this.ani_viewed.TabStop = true;
             this.ani_viewed.Text = "Просмотренно";
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(165, -1);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(2, 451);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(165, 47);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(636, 2);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // LoginBox
+            // 
+            this.LoginBox.Location = new System.Drawing.Point(28, 29);
+            this.LoginBox.Name = "LoginBox";
+            this.LoginBox.Size = new System.Drawing.Size(100, 20);
+            this.LoginBox.TabIndex = 9;
+            this.LoginBox.Visible = false;
+            // 
+            // PassBox
+            // 
+            this.PassBox.Location = new System.Drawing.Point(28, 78);
+            this.PassBox.Name = "PassBox";
+            this.PassBox.Size = new System.Drawing.Size(100, 20);
+            this.PassBox.TabIndex = 10;
+            this.PassBox.Visible = false;
+            // 
+            // Login_btn
+            // 
+            this.Login_btn.Location = new System.Drawing.Point(41, 104);
+            this.Login_btn.Name = "Login_btn";
+            this.Login_btn.Size = new System.Drawing.Size(75, 23);
+            this.Login_btn.TabIndex = 11;
+            this.Login_btn.Text = "Войти";
+            this.Login_btn.UseVisualStyleBackColor = true;
+            this.Login_btn.Visible = false;
+            this.Login_btn.Click += new System.EventHandler(this.Login_btn_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.ani_viewed);
             this.Controls.Add(this.ani_look2);
             this.Controls.Add(this.ani_look);
-            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.anime);
             this.Controls.Add(this.your_list);
             this.Controls.Add(this.user_name);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.LoginBox);
+            this.Controls.Add(this.PassBox);
+            this.Controls.Add(this.Login_btn);
             this.Name = "Form1";
             this.Text = "Anime Manga List";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -136,10 +205,16 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label user_name;
         private System.Windows.Forms.Label your_list;
-        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel anime;
         private System.Windows.Forms.LinkLabel ani_look;
         private System.Windows.Forms.LinkLabel ani_look2;
         private System.Windows.Forms.LinkLabel ani_viewed;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox LoginBox;
+        private System.Windows.Forms.TextBox PassBox;
+        private System.Windows.Forms.Button Login_btn;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- animelist/Form1.cs
+++ animelist/Form1.cs
@@ -1,24 +1,50 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Runtime.CompilerServices;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
 
 namespace animelist
 {
     public partial class Form1 : Form
     {
+        
 
         public Form1()
         {
-            InitializeComponent();
             
+            InitializeComponent();
+            /*
+            bool user = false;
+
+            if(user == true)
+            {
+                string usericon = "";
+                SqlConnection connn = new SqlConnection("Server=DESKTOP-DEQB5OG\\SQLEXPRESS;Database=test_ptog;Trusted_Connection=True;");
+                connn.Open();
+                SqlCommand c = new SqlCommand($"SELECT * FROM user WHERE icon = @icon", connn);
+                c.Parameters.Add(new SqlParameter("@icon",));
+                SqlDataReader dr;
+                dr = c.ExecuteReader();
+                
+            }
+            */
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             //Изображение пользователя - 
             pictureBox1.ImageLocation = "https://sithond.ru/404.png";
+            pictureBox2.ImageLocation = "https://sithond.ru/aav.png";
+            pictureBox3.ImageLocation = "https://sithond.ru/aav.png";
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -28,7 +54,43 @@ namespace animelist
 
         private void label1_Click(object sender, EventArgs e)
         {
+            user_name.Visible = false;
+            ani_look.Visible = false;
+            ani_look2.Visible = false;
+            ani_viewed.Visible = false;
+            your_list.Visible = false;
+            anime.Visible = false;
+            pictureBox1.Visible = false;
 
+            LoginBox.Visible = true;
+            PassBox.Visible = true;
+            Login_btn.Visible = true;
+        }
+
+        private void Login_btn_Click(object sender, EventArgs e)
+        {
+            if (LoginBox.Text == "a" && PassBox.Text == "a")
+            {
+                bool user = true;
+
+
+                user_name.Visible = true;
+                ani_look.Visible = true;
+                ani_look2.Visible = true;
+                ani_viewed.Visible = true;
+                your_list.Visible = true;
+                anime.Visible = true;
+                pictureBox1.Visible = true;
+
+                LoginBox.Visible = false;
+                PassBox.Visible = false;
+                Login_btn.Visible = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
         }
     }
 }
